dc52df241ab4f86e5dd863a025772c0c87ffdf04
Josh Benham <joshbenham@gmail.com>
2013-04-18 11:42:03 +1000

    Removed all aspects of twitter bootstrap

--- functions.php
+++ functions.php
@@ -2,8 +2,7 @@
 
 function skeleton_scripts()
 {
-	wp_register_script('bootstrap', get_template_directory_uri() . '/js/bootstrap-2.2.2.min.js', array( 'jquery' ) );
-	wp_enqueue_script('bootstrap');
+
 }
 add_action('wp_enqueue_scripts', 'skeleton_scripts');
 
