93b883910de9999bac48d174494da903b5a00226
Josh Benham <joshbenham@gmail.com>
2013-04-19 10:34:24 +1000

    Updated foundation and added an actual CSS architecture

--- functions.php
+++ functions.php
@@ -26,12 +26,18 @@ function skeleton_styles() {
 	
 	wp_register_style('helpers', get_template_directory_uri() . '/css/helpers.css');
 	wp_enqueue_style('helpers');
+
+	wp_register_style('layout', get_template_directory_uri() . '/css/layout.css');
+	wp_enqueue_style('layout');
 	
 	wp_register_style('forms', get_template_directory_uri() . '/css/forms.css');
 	wp_enqueue_style('forms');
 	
-	wp_register_style('default', get_template_directory_uri() . '/css/default.css');
-	wp_enqueue_style('default');
+	wp_register_style('modules', get_template_directory_uri() . '/css/modules.css');
+	wp_enqueue_style('modules');
+	
+	wp_register_style('custom', get_template_directory_uri() . '/css/custom.css');
+	wp_enqueue_style('custom');
 }
 
 if (function_exists('register_sidebar'))
