e77267ff481f9ceffd3aa85711436ca15fbc03d9
Josh Benham <joshbenham@gmail.com>
2013-05-06 09:43:53 +1000

    Updated the comments in the files

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+/* ------------------------------------------------------------------ *\
+Component: Main;
+Version: 1.0.0;
+Author: Josh Benham;
+Author URI: http://github.com/joshbenham;
+
+Base styles for the modules
+\* ------------------------------------------------------------------ */
+
 ;(function($) {
     "use strict";
 }(jQuery));
