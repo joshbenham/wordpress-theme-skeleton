b785fcc9b0922e1f46d1534f4c16dcfd5e734b50
Josh Benham <joshbenham@gmail.com>
2013-06-12 17:30:17 +1000

    Added a simple button shortcode to use a foundation button

--- functions/shortcodes.php
+++ functions/shortcodes.php
@@ -1,1 +1,19 @@
 <?php
+
+
+/* GENERIC ---------------------------------------------------------- */
+
+
+add_shortcode('button', 'jb_button');
+function jb_button($atts) {
+	extract(shortcode_atts(array(
+		'id' => '1',
+		'text' => 'Submit'
+	), $atts));
+
+	return sprintf(
+		'<a href="%s" class="button">%s</a>',
+		get_permalink($id),
+		$text
+	);
+}
