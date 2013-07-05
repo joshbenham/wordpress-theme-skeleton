1b51b2b1638a894bbac9fd483407d008210d55ac
Josh Benham <joshbenham@gmail.com>
2013-07-05 15:26:47 +1000

    Removed description as Wordpress SEO now adds it

--- header.php
+++ header.php
@@ -8,7 +8,6 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<title><?php wp_title( '|', true, 'right' ); ?></title>
 		<meta name="keywords" content="">
-		<meta name="description" content="">
 		<meta name="viewport" content="width=device-width">
 
 		<link rel="profile" href="http://gmpg.org/xfn/11" />
@@ -26,7 +25,6 @@
 		<!--[if lt IE 7]>
 			<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
 		<![endif]-->
-
 
 		<div class="row">
 			<header role="banner" class="header large-12 columns">
