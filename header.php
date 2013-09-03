42e6713574710df96cbfc43741a3e0d18d73212e
Josh Benham <joshbenham@gmail.com>
2013-09-03 16:30:55 +1000

    Removed unneeded meta from the head

--- header.php
+++ header.php
@@ -10,9 +10,6 @@
 		<meta name="keywords" content="">
 		<meta name="viewport" content="width=device-width">
 
-		<link rel="profile" href="http://gmpg.org/xfn/11" />
-		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-
 		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url');?>">
 
 		<?php wp_head(); ?>
--- includes/functions.php
+++ includes/functions.php
@@ -2,3 +2,15 @@
 
 
 /* FUNCTIONS -------------------------------------------------------- */
+
+
+/* CLEANUP HEAD META ------------------------------------------------ */
+
+
+add_action('init', 'cleanup_head');
+function cleanup_head() {
+	remove_action('wp_head', 'rsd_link');
+	remove_action('wp_head', 'wlwmanifest_link');
+	remove_action('wp_head', 'wp_generator');
+}
+
