88c4d3c0a496b5ff549cf4f65b5a5c01c614b5e3
Josh Benham <joshbenham@gmail.com>
2013-09-11 11:54:05 +1000

    Removed the old button shortcode

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -3,17 +3,3 @@
 
 /* GENERIC ---------------------------------------------------------- */
 
-
-add_shortcode('button', 'jb_button');
-function jb_button($atts) {
-	extract(shortcode_atts(array(
-		'id' => '1',
-		'text' => 'Submit'
-	), $atts));
-
-	return sprintf(
-		'<a href="%s" class="button">%s</a>',
-		get_permalink($id),
-		$text
-	);
-}
