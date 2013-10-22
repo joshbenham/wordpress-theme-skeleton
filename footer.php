d607b63e2fb9e4e4da671115dfc09bba5bfc92a8
Josh Benham <joshbenham@gmail.com>
2013-10-22 15:21:20 +1100

    Starting on a navigations module

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 			<div class="row">
 
 				<div class="large-6 columns">
-					<div class="navigation-inline">&copy; Copyright <?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'container_class' => 'menu-container' ) ); ?></div>
+					<div class="navigation--inline">&copy; Copyright <?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'container_class' => 'menu-container' ) ); ?></div>
 				</div>
 
 				<div class="large-6 columns">
